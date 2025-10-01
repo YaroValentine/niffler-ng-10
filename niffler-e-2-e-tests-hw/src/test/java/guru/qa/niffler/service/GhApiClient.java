3ad41a7b5b98f9341301b55c1cbf030eeed85934
yarov <yaroslav.vinokurov@gmail.com>
2025-10-01 17:49:59 -0400

    3.2. bug fix

--- niffler-e-2-e-tests-hw/src/test/java/guru/qa/niffler/service/GhApiClient.java
+++ niffler-e-2-e-tests-hw/src/test/java/guru/qa/niffler/service/GhApiClient.java
@@ -14,18 +14,20 @@ public class GhApiClient {
   private static final String GH_TOKEN_ENV = "GITHUB_TOKEN";
 
   private final Retrofit retrofit = new Retrofit.Builder()
-      .baseUrl(Config.getInstance().ghUrl())
-      .addConverterFactory(JacksonConverterFactory.create())
-      .build();
+    .baseUrl(Config.getInstance().ghUrl())
+    .addConverterFactory(JacksonConverterFactory.create())
+    .build();
 
   private final GhApi ghApi = retrofit.create(GhApi.class);
 
   @SneakyThrows
   public String issueState(String issueNumber) {
     JsonNode responseBody = ghApi.issue(
-        "Bearer " + System.getenv(GH_TOKEN_ENV),
-        issueNumber
+      "Bearer " + System.getenv(GH_TOKEN_ENV),
+      issueNumber
     ).execute().body();
+    System.out.println("ENV GITHUB_TOKEN=" + System.getenv("GITHUB_TOKEN"));
+    System.out.println("PROP github.token=" + System.getProperty("github.token"));
     return Objects.requireNonNull(responseBody).get("state").asText();
   }
 }
